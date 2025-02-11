eae8f40b8bcd9a4e031e3cda5ef6d37a2c65849b
jefferson guedes <jefferson.guedes@sepog.fortaleza.ce.gov.br>
2025-02-11 16:50:40 -0300

    :bug: Fix: Ajustando tempo de access Token

--- core/settings.py
+++ core/settings.py
@@ -116,7 +116,7 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
 }
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=3000),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
 
